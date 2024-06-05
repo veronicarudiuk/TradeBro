90cbd059114a224fe4621cc170e387497e601ea5
rudvLDT <123957835+rudvLDT@users.noreply.github.com>
2024-06-05 15:20:10 +0400

    Initial Commit

---
+++ TradeBro/TradeBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeBroApp.swift
+//  TradeBro
+//
+//  Created by Veronica Rudiuk on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TradeBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
